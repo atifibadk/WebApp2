a10154079a4d5c27ab2af811d43bb8e8db787b69
ymxplusC <atifibadk@gmail.com>
2021-03-26 19:16:57 +0530

    revisio2

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+      id: 1,
+      pic:
+        "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
+      name: "Flat-Twist With Twist Out",
+      Role: "Medium",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
+    }
+  ];
+  
+  export default data;
+  
--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const div = document.createElement("div")
-const h4 = document.createElement("h4")
-const a = document.createElement("a")
-const img = document.createElement("img")
+import data from "./data.js";
 
-const body = document.querySelector('body')
 
-body.append(div)
-h4.append(a)
-div.append(h4)
-div.append(img)
+const buildPlayerCard = (player) => {
+    const div = document.createElement("div")
+    const h4 = document.createElement("h4")
+    const a = document.createElement("a")
+    const img = document.createElement("img")
 
-a.innerHTML="First Hairstyle"
+    const body = document.querySelector('body')
+
+    body.append(div)
+    h4.append(a)
+    div.append(h4)
+    div.append(img)
+
+    div.setAttribute('class','card')
+
+    a.innerHTML = player.name
+    a.setAttribute('href',player.info)
+    img.setAttribute('src',player.pic)
+
+}
+
+data.forEach(player => buildPlayerCard(player))
+
+
+
+
+
+
